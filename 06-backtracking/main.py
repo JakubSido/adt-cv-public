dd3e733fc5aa2e8c1008eb599b58c161ab91e0e2
Jakub Sido <JakubSido@gmail.com>
2024-04-08 08:47:25 +0200

    reference refactor

--- 06-backtracking/main.py
+++ 06-backtracking/main.py
@@ -1,17 +1,11 @@
 import random
 import numpy as np
-
-EMPTY_VALUE = 0
+import os
 
 class SudokuSolver:
 
-    def __init__(self, field:np.ndarray|None=None, n_rows:int=9, n_columns:int=9):
-
-        if field is None:
-            field = np.zeros([n_rows, n_columns], dtype=int)
-        self.field = field
-        self.n_rows = n_rows
-        self.n_columns = n_columns
+    def __init__(self):
+        self.field = np.zeros([9, 9], dtype=int)
 
 
     def check_sequence(self, sequence:np.ndarray):
@@ -26,13 +20,12 @@ class SudokuSolver:
         pass
 
     def check_column(self, column_index:int):
-        pass
+        pass 
 
     def check_block(self, row_index:int, column_index:int):
-        pass
+        pass 
 
     def solve(self): 
-        
         return False 
 
     def check_field(self):
@@ -42,10 +35,6 @@ class SudokuSolver:
 
 def main():    
     sudoku_solver = SudokuSolver()
-    # sudoku_solver.load("sudoku.csv")
-    
-    print(sudoku_solver.solve())
-    print(sudoku_solver.field)
     
 if __name__ == "__main__":
     main()
--- 06-backtracking/main_ref.py
+++ 06-backtracking/main_ref.py
@@ -1,16 +1,11 @@
 import random
 import numpy as np
 import os
-EMPTY_VALUE = 0
 
 class SudokuSolver:
 
-    def __init__(self, n_rows:int=9, n_columns:int=9):
-
-        self.field = np.zeros([n_rows, n_columns], dtype=int)
-         
-        self.n_rows = n_rows
-        self.n_columns = n_columns
+    def __init__(self):
+        self.field = np.zeros([9, 9], dtype=int)
 
     
     def load(self, file_path:str):
@@ -22,7 +17,7 @@ class SudokuSolver:
 
     def save(self, file_path:str):
         with open(file_path, "w", encoding="utf-8") as f:
-            for row_index in range(self.n_rows):
+            for row_index in range(9):
                 row = self.field[row_index]
                 f.write(";".join([str(number) for number in row]) + "\n")
     
@@ -73,7 +68,7 @@ class SudokuSolver:
         
         for r in range(9):
             for c in range(9):
-                if self.field[r][c] != EMPTY_VALUE:  
+                if self.field[r][c] != 0:  
                     if r == 8 and c == 8:  # mame vyreseno
                         return True
                     continue
@@ -87,21 +82,21 @@ class SudokuSolver:
                     if solved:
                         return True
                     
-                self.field[r][c] = EMPTY_VALUE
+                self.field[r][c] = 0
                 return False
         
 
     def check_field(self):
         
-        for r in range(self.n_rows):
+        for r in range(9):
             if not self.check_row(r):
                 return False
-        for c in range(self.n_columns):
+        for c in range(9):
             if not self.check_column(c):
                 return False
 
-        for r in range(0, self.n_rows, 3):
-            for c in range(0, self.n_columns, 3):
+        for r in range(0, 9, 3):
+            for c in range(0, 9, 3):
                 if not self.check_block(r, c):
                     return False
         
@@ -111,13 +106,14 @@ class SudokuSolver:
 
 def main():    
     sudoku_solver = SudokuSolver()
-
+    
     for sudoku_file in os.listdir("data"):
         file = os.path.join("data", sudoku_file)
         print("solving: ", file)
         sudoku_solver.load(file)
         print(sudoku_solver.solve())
         print(sudoku_solver.field)
+    
     
 if __name__ == "__main__":
     main()
